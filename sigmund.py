0eca778338318662dada4c8014a91a13c97ad9db
COOL-SIGMA-TOM-REALEST-SIGMA-MALE <149688826+COOL-SIGMA-TOM-REALEST-SIGMA-MALE@users.noreply.github.com>
2023-11-07 12:19:42 +0000

    Add files via upload

---
+++ sigmund.py
@@ -0,0 +1,8 @@
+bongus = ['mug','cup','ron','skibidi','ron ronnie','wojtek', 'sigma','bogdan']
+for i in bongus:
+    if i == 'sigma':
+        print('male')
+        break
+    if i == 'bogdan':
+        print('sigma nation rise up')
+ 
